ace7bd6b733a2bffae5d98c57ebf650582b86932
Bhanuprakash_Reddy_ML <Bhanuprakash.reddy@maplelabs.com>
2023-11-15 13:31:30 +0530

    Changed the artifact ID to io.jenkins.plugins

---
+++ src/main/java/io/jenkins/plugins/Events/BuildEventListener.java
@@ -0,0 +1,62 @@
+package io.jenkins.plugins.Events;
+
+import java.util.logging.Logger;
+import java.util.concurrent.TimeUnit;
+
+import javax.annotation.Nonnull;
+
+import hudson.Extension;
+import hudson.model.Run;
+import hudson.model.Queue;
+import hudson.model.TaskListener;
+import hudson.model.listeners.RunListener;
+
+import io.jenkins.plugins.Utils;
+import io.jenkins.plugins.Events.interfaces.IBuildEvent;
+import io.jenkins.plugins.Events.Collector.BuildEventCollectorImpl;
+
+@Extension
+public class BuildEventListener extends RunListener<Run> {
+    private IBuildEvent buildCollector;
+    private static final Logger logger = Logger.getLogger(BuildEventListener.class.getName());
+
+    @Override
+    public void onStarted(Run run, TaskListener listener) {
+        try {
+            logger.info("Start BuildListener#onStarted");
+            buildCollector = new BuildEventCollectorImpl(run, listener);
+            buildCollector.collectEventData(IBuildEvent.Type.STARTED);
+            Utils.sendEvent(buildCollector);
+            // waiting time
+            Queue queue = Queue.getInstance();
+            Queue.Item item = queue.getItem(run.getQueueId());
+            try {
+                long waitingMs = (Utils.getCurrentTimeInMillis() - item.getInQueueSince());
+                logger.info("Job waiting time: " + TimeUnit.MILLISECONDS.toSeconds(waitingMs));
+            } catch (RuntimeException e) {
+                logger.warning("Unable to compute 'waiting' metric. " +
+                        "item.getInQueueSince() unavailable, possibly due to worker instance provisioning");
+            }
+
+            logger.info("End BuildListener#onStarted");
+        } catch (Exception e) {
+            logger.severe("Failed to process build start : "+e.toString());
+        }
+    }
+
+    /**
+     * This function will send job status to client
+     */
+    @Override
+    public void onCompleted(Run run, @Nonnull TaskListener listener) {
+        try {
+            logger.info("Start BuildListener#onCompleted");
+            buildCollector = new BuildEventCollectorImpl(run, listener);
+            buildCollector.collectEventData(IBuildEvent.Type.COMPLETED);
+            Utils.sendEvent(buildCollector);
+            logger.info("End BuildListener#onCompleted");
+        } catch (Exception e) {
+            logger.severe("Failed to process build completion : "+e.toString());
+        }
+    }
+}
---
+++ src/main/java/io/jenkins/plugins/Metrics/MetricsCollector.java
@@ -0,0 +1,68 @@
+package io.jenkins.plugins.Metrics;
+
+import java.util.logging.Logger;
+
+import hudson.Extension;
+import hudson.model.Computer;
+import jenkins.model.Jenkins;
+import hudson.model.PeriodicWork;
+import hudson.model.AsyncPeriodicWork;
+import hudson.model.TaskListener;
+import java.io.IOException;
+import org.kohsuke.stapler.DataBoundConstructor;
+
+import io.jenkins.plugins.Utils;
+import io.jenkins.plugins.Metrics.Data.NodeMetricsImpl;
+import io.jenkins.plugins.Metrics.Data.QueueMetricsImpl;
+import io.jenkins.plugins.Metrics.Data.JenkinsMetricsImpl;
+import io.jenkins.plugins.Metrics.interfaces.IPublishMetrics;
+
+
+@Extension
+public class MetricsCollector extends AsyncPeriodicWork {
+    private static final IPublishMetrics nodeMetrics = new NodeMetricsImpl();
+    private static final IPublishMetrics queueMetrics = new QueueMetricsImpl();
+    private static final IPublishMetrics jenkinsMetrics = new JenkinsMetricsImpl();
+    private static final Logger logger = Logger.getLogger(MetricsCollector.class.getName());
+    
+    @DataBoundConstructor
+    public MetricsCollector() {
+        super("Metrics_Collector");       
+    }
+
+    @Override
+    public long getRecurrencePeriod() {
+        // Convert the collection interval from minutes to milliseconds
+        int recurrancePeriodInMillis =  (Utils.getGlobalDescriptor().getReportingInterval()) * 60 * 1000;
+        return recurrancePeriodInMillis;        
+
+    }   
+
+    @Override        
+    // protected void execute(TaskListener listener) throws IOException, InterruptedException {
+    protected void execute(TaskListener listener) {        
+        if (Utils.getGlobalDescriptor().getIsMetricEnabled()) {
+            try {
+                logger.info("Start Computing metrics");
+                Jenkins instance = Jenkins.getInstanceOrNull();
+
+                if (instance != null) {
+                    Utils.sendMetrics(queueMetrics.collectMetrics(instance));
+                    Utils.sendMetrics(jenkinsMetrics.collectMetrics(instance));
+                    Computer[] computers = instance.getComputers();
+                    if (computers != null)
+                        Utils.sendMetrics(nodeMetrics.collectMetrics(computers));
+                    else
+                        logger.warning("couldn't retrieve computers");
+                } else {
+                    logger.warning("Instance is null");
+                }
+                logger.info("End Computing metrics");
+            } catch (Exception e) {
+                logger.severe("Failed to compute and send Jenkins metrics: " + e.toString());
+            }
+        } else {
+            logger.fine("Metric reporting is not enabled");
+        }        
+    }
+}
